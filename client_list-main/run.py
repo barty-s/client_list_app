243d89d5e82ba036ef69181f5e4206e7af6cb56c
barty-s <117319197+barty-s@users.noreply.github.com>
2023-10-02 17:47:27 +0200

    Add exit program option to client_list_menu()

--- client_list-main/run.py
+++ client_list-main/run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import pyinputplus as pyip
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,17 +27,17 @@ def new_client_data():
     """
     print("Let's add a new client\n")
 
-    name = input("Name: ")
-    email = pyip.inputEmail("Email address: ")
-    age = pyip.inputInt("Age: ", min=18)
-    print("Goal distance: ")
+    name = input("Name: \n")
+    email = pyip.inputEmail("Email address: \n")
+    age = pyip.inputInt("Age: \n", min=18)
+    print("Goal distance: \n")
     distance = pyip.inputMenu(
-        ["5km", "10km", "half-marathon", "marathon"], numbered=True
+        ["5km", "10km", "half-marathon", "marathon" "\n"], numbered=True
     )
-    current_pb = str(pyip.inputTime("Current PB to nearest minute as hh:mm : "))
-    next_race = str(pyip.inputDate("Date of next race as mm/dd/yyyy: "))
+    current_pb = str(pyip.inputTime("Current PB to nearest minute as hh:mm : \n"))
+    next_race = str(pyip.inputDate("Date of next race as mm/dd/yyyy: \n"))
     goal_time = str(
-        pyip.inputTime("Goal time for next race to nearest minute as hh:mm : ")
+        pyip.inputTime("Goal time for next race to nearest minute as hh:mm : \n")
     )
 
     client_data = [name, email, age, distance, current_pb, next_race, goal_time]
@@ -57,7 +58,7 @@ def search_client_email():
     Searches for client using their email address
     """
     try:
-        client_email = pyip.inputEmail("Search by client's email address: ")
+        client_email = pyip.inputEmail("Search by client's email address: \n")
         email_list = running_worksheet.col_values(2)
         client_index = email_list.index(client_email)
         return client_index
@@ -88,9 +89,13 @@ def delete_client_data(data):
 
 
 def client_list_menu():
+    """
+    Displays a list of actions for the user to choose from i.e add, display, delete client or exit program.
+    The main menu is displayed after each action.
+    """
     print("What you like to do? Type a number from the list below:\n")
     actions = pyip.inputMenu(
-        ["Add a client", "Display a client", "Delete a client"],
+        ["Add a client", "Display a client", "Delete a client", "Exit" "\n"],
         numbered=True,
     )
 
@@ -98,24 +103,27 @@ def client_list_menu():
         new_client = new_client_data()
         add_new_client_to_worksheet(new_client)
         print(new_client)
+        client_list_menu()
     elif actions == "Display a client":
         searched_client_index = search_client_email()
         display_client_data(searched_client_index)
+        client_list_menu()
     elif actions == "Delete a client":
         searched_client_index = search_client_email()
         delete_client_data(searched_client_index)
+        client_list_menu()
+    elif actions == "Exit":
+        print("See you next time")
+        sys.exit()
+
+
+# def clear_screen(): os.system("cls")
 
 
 def main():
     """
-    Runs all functions in the program
+    Run functions to display list of actions in the program
     """
-    # new_client = new_client_data()
-    # add_new_client_to_worksheet(new_client)
-    # print(new_client)
-    # searched_client_index = search_client_email()
-    # display_client_data(searched_client_index)
-    # delete_client_data(searched_client_index)
     client_list_menu()
 
 
